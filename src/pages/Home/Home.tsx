23b7b9b29fe87e0c06d2a28b73c6f35cf7c6f139
Leandro Reis <leandro.reiz@gmail.com>
2022-05-08 20:14:18 +0100

    Finish home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,18 +1,18 @@
 function Home() {
   return (
     <>
-      <main className="home">
+      <main className="home grid">
         <div className="home-content">
-          <h5 className="heading subtitle">So, you want to travel to</h5>
-          <h1 className="heading">Space</h1>
-          <p className="body-text">
+          <h5 className="heading subtitle mb-2">So, you want to travel to</h5>
+          <h1 className="heading mb-3">Space</h1>
+          <p className="body-text text-blue">
             Let's face it; if you want to go to space, you might as well
             genuinely go to outer space and not hover kind of on the edge of it.
             Well sit back, and relax because we'll give you a truly out of this
             world experience!
           </p>
         </div>
-        <div className="explore-btn">Explore</div>
+        <button className="explore-btn">Explore</button>
       </main>
     </>
   );
